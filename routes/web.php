b613cfdee866d25e884d5313667e3dd549b0b772
ezrarexy <35161093+ezrarexy@users.noreply.github.com>
2022-12-13 11:31:05 +0700

    3rd commit
    Full module

--- routes/web.php
+++ routes/web.php
@@ -17,31 +17,37 @@ use App\Http\Controllers\adminPageController;
 |
 */
 
+Auth::routes();
+
 Route::group(['domain' => 'xmanage.localhost'], function () {
-    Route::get('/', [adminPageController::class, 'Index']);
-    Route::get('/cars', [adminPageController::class, 'Car'])->name('xmCars');
-    Route::get('/brands', [adminPageController::class, 'Brand'])->name('xmBrands');
-    Route::get('/categories', [adminPageController::class, 'Category']);
-    Route::get('/coba', [adminPageController::class, 'Coba']);
-    Route::POST('/car/in', [adminController::class, 'CarIn'])->name('carIn');
-    Route::POST('/brand/in', [adminController::class, 'BrandIn'])->name('brandIn');
-    Route::POST('/category/in', [adminController::class, 'CategoryIn'])->name('categoryIn');
+    Route::group(['middleware' => ['web', 'auth']], function () {
+        Route::get('/', [adminPageController::class, 'Index']);
+        Route::get('/cars', [adminPageController::class, 'Car'])->name('xmCars');
+        Route::get('/brands', [adminPageController::class, 'Brand'])->name('xmBrands');
+        Route::get('/categories', [adminPageController::class, 'Category']);
+        Route::get('/coba', [adminPageController::class, 'Coba']);
+        Route::POST('/car/in', [adminController::class, 'CarIn'])->name('carIn');
+        Route::POST('/brand/in', [adminController::class, 'BrandIn'])->name('brandIn');
+        Route::POST('/category/in', [adminController::class, 'CategoryIn'])->name('categoryIn');
+    });
 });
 
-Route::get('/', [pageController::class, 'HomePage']);
+    Route::get('/', [pageController::class, 'HomePage']);
 
-Route::get('/car', [pageController::class, 'Car']);
+    Route::get('/car', [pageController::class, 'Car']);
 
-Route::get('/car/{con}', [pageController::class, 'Cars']);
+    Route::get('/car/{con}', [pageController::class, 'Cars']);
 
-Route::get('/brand', [pageController::class, 'Brand']);
+    Route::get('/brand', [pageController::class, 'Brand']);
 
-Route::get('/tag', [pageController::class, 'Tag']);
+    Route::get('/tag', [pageController::class, 'Tag']);
 
-Route::get('/contact', [pageController::class, 'Contact']);
+    Route::get('/contact', [pageController::class, 'Contact']);
 
-Route::get('/product/{id}', [pageController::class, 'Product']);
+    Route::get('/product/{id}', [pageController::class, 'Product']);
 
-Route::get('/about', [pageController::class, 'About']);
+    Route::get('/about', [pageController::class, 'About']);
 
-Route::get('/test', [pageController::class, 'Test']);
+    Route::get('/test', [pageController::class, 'Test']);
+
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
